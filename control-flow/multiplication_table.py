4f1fb58fcfd279629d5364cdabef65e93eb3d855
Amanuel <amanuelwendimu19@gmail.com>
2025-11-15 15:03:17 +0300

    Multiplication table gnerator

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,3 @@
+number = int(input("Enter a numner to see its multiplication table:"))
+for y in range(1,11):
+    print(f"{number} * {y} = {number*y}")
---
+++ control-flow/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+number = int(input("Enter a numner to see its multiplication table:"))
