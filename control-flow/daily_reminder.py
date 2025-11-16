1b33e6807b9861fe2b7eae657e40d7e6bc19c9d1
Amanuel <amanuelwendimu19@gmail.com>
2025-11-16 14:16:27 +0300

    update daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,25 +1,28 @@
 task = input("Enter your task: ")
-priority = input("Priority (high/medium/low):").lower()
-time_bound = input("Is it time-bound? (yes/no):").lower()
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 
-while priority not in ["high","medium","low"]:
+while priority not in ["high", "medium", "low"]:
     print("Invalid priority. Please enter high, medium, or low.")
     priority = input("Priority (high/medium/low): ").lower()
+
 match priority:
     case "high":
-        reminder = f"Reminder: '{task}' is a high priority task "
+        reminder = f"Reminder: '{task}' is a high priority task"
     case "medium":
-        reminder = f"Reminder: '{task}' is a medium priority task "
+        reminder = f"Reminder: '{task}' is a medium priority task"
     case "low":
-        reminder = f"Note: '{task}' is a low priority task "
+        reminder = f"Note: '{task}' is a low priority task"
     case _:
         reminder = f"'{task}' has an undefined priority."
-    
+
+
 if time_bound == "yes":
-    reminder += "that requires immediate attention today!"
+    reminder += " that requires immediate attention today!"
 else:
     if priority == "low":
-        reminder += ". consider competing it when you have free time."
+        reminder += ". Consider completing it when you have free time."
     else:
         reminder += "."
+
 print(reminder)
